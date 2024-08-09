f7102bbd55dba7450f949c0c140c893bb4888bec
DiranX <firdi.fikar.fkr@gmail.com>
2024-08-09 08:35:32 +0700

    Revert "PIP-level"
    This reverts commit 7b89c2bec9b0f58312eb84f146ae10e139a364e8.

--- Assets/Scripts/FIrdi/Dragable.cs
+++ Assets/Scripts/FIrdi/Dragable.cs
@@ -8,24 +8,10 @@ public class Dragable : MonoBehaviour
     public bool sameTag;
     public Vector2 startPosition;
     public Vector2 answerPos;
-    public Transform rightAnswerLocation;
-    public string tagName;
-    public int score;
 
     private void Start()
     {
         startPosition = gameObject.transform.position;
-    }
-    private void Update()
-    {
-        if (this.score <= 0)
-        {
-            this.score = 0;
-        }
-        else if( this.score >= 1)
-        {
-            this.score = 1;
-        }
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -42,20 +28,12 @@ public class Dragable : MonoBehaviour
             isTag = true;
             answerPos = collision.gameObject.transform.position;
         }
-        if (collision.gameObject.CompareTag(tagName))
-        {
-            Score.instance.PlusScore(1);
-        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("answer"))
         {
             isTag = false;
-        }
-        if (collision.gameObject.CompareTag(tagName))
-        {
-            Score.instance.MinusScore(1);
         }
     }
 }
